8b9f6e288d166be29b74f41c220c685f697f0c4b
Arun Kumar <arunprajapati9825@gmail.com>
2024-10-28 20:18:32 +0530

    cache handler and deployment script for Next.js app, observability through instrumentation and Nginx configuration

---
+++ instrumentation.ts
@@ -0,0 +1,8 @@
+/*
+The instrumentation.js|ts file is used to integrate observability tools into your application,
+ allowing you to track the performance and behavior, and to debug issues in production.
+ - https://nextjs.org/docs/app/api-reference/file-conventions/instrumentation
+ - regiter() fun will run once when we start the server. 
+ - it is usefull for logging, tracing, and performance monitoring , running scripts etc.
+*/
+export async function register() {}
--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	// Recommended: this will reduce output
+	// Docker image size by 80%+
 	output: "standalone",
+
+	// Optional: bring your own cache handler
+	// cacheHandler: path.resolve('./cache-handler.mjs'),
+	// cacheMaxMemorySize: 0, // Disable default in-memory caching
 
 	// Nginx will do gzip compression. We disable
 	// compression here so we can prevent buffering
 	// streaming responses
 	compress: false,
+	// Optional: override the default (1 year) `stale-while-revalidate`
+	// header time for static pages
+	// swrDelta: 3600 // seconds
 };
 
 export default nextConfig;
